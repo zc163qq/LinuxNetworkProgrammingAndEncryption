48145f6803719715cde9a69e34ee591ef97f460a
TakoTakoo <85894051+TakoTakoo@users.noreply.github.com>
2021-06-21 06:40:50 +0800

    update about & add signal sample

---
+++ docs/src/network/advanced_io/sig.c
@@ -0,0 +1,60 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/epoll.h>
+#include <termios.h>
+#include <unistd.h>
+
+static volatile sig_atomic_t gotSigio = 0;
+
+static void handler(int sig) { gotSigio = 1; }
+
+int main(int argc, char *argv[]) {
+  int flags, j, cnt;
+  struct termios origTermios;
+  char ch;
+  struct sigaction sa;
+  int done;
+
+  /* Establish handler */
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = SA_RESTART;
+  sa.sa_handler = handler;
+  if (sigaction(SIGIO, &sa, NULL) == -1) {
+    perror("sigaction()\n");
+    exit(1);
+  }
+
+  /* Set owner process */
+  if (fcntl(STDIN_FILENO, F_SETOWN, getpid()) == -1) {
+    perror("fcntl() / F_SETOWN\n");
+    exit(1);
+  }
+
+  /* Enable "I/O possible" signaling and make I/O nonblocking */
+  flags = fcntl(STDIN_FILENO, F_GETFL);
+  if (fcntl(STDIN_FILENO, F_SETFL, flags | O_ASYNC | O_NONBLOCK) == -1) {
+    perror("fcntl() / F_SETFL\n");
+    exit(1);
+  }
+
+  for (done = 0, cnt = 0; !done; cnt++) {
+    sleep(1);
+
+    if (gotSigio) {
+      gotSigio = 0;
+
+      /* Read all available input until error (probably EAGAIN)
+         or EOF */
+      while (read(STDIN_FILENO, &ch, 1) > 0 && !done) {
+        printf("cnt=%d; read %c\n", cnt, ch);
+        done = ch == '#';
+      }
+    }
+  }
+  exit(0);
+}
